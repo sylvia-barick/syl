44d06e0e33d7cdf4957b6344ef21fb6b67d92a89
Sylvia Barick <Sylvia.Barick2024@iem.edu.in>
2026-02-28 04:00:04 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-
-  // 👇 REQUIRED for GitHub Pages + custom domain
-  basePath: '',
-  assetPrefix: '/',
-
   images: {
     unoptimized: true,
   },
